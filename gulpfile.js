5f7bb603efe8d9b254c2f43295c5ac09f93678fe
thuy-n3 <ngtan3@gmail.com>
2019-05-15 16:31:51 -0500

    build

--- gulpfile.js
+++ gulpfile.js
@@ -12,6 +12,7 @@ const plumber = require("gulp-plumber");
 const rename = require("gulp-rename");
 const sass = require("gulp-sass");
 const uglify = require("gulp-uglify");
+const deploy = require("gulp-gh-pages");
 
 // Load package.json for banner
 const pkg = require('./package.json');
@@ -125,6 +126,12 @@ const vendor = gulp.series(clean, modules);
 const build = gulp.series(vendor, gulp.parallel(css, js));
 const watch = gulp.series(build, gulp.parallel(watchFiles, browserSync));
 
+//Push build to gh-pages
+gulp.task('deploy', function () {
+  return gulp.src("./dist/**/*")
+    .pipe(deploy())
+});
+
 // Export tasks
 exports.css = css;
 exports.js = js;
@@ -133,3 +140,5 @@ exports.vendor = vendor;
 exports.build = build;
 exports.watch = watch;
 exports.default = build;
+
+
